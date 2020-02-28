f01c1d245e6e92401834fe9205025faedee2b515
reanjelica-p <59627740+reanjelica-p@users.noreply.github.com>
2020-02-27 18:29:33 -0700

    Update D - Simple Joins Exercise.sql

--- SQL/D - Simple Joins Exercise.sql
+++ SQL/D - Simple Joins Exercise.sql
@@ -47,11 +47,7 @@ GROUP BY FirstName, LastName, CourseId
 ORDER BY 'Staff Full Name', CourseId
 
 --3.	Select all the Club ID's and the Student full names that are in them
-SELECT  FirstName + ' ' + LastName AS 'Full Name',
-        ClubId
-FROM    Student
-    INNER JOIN Club
-        ON Club.ClubId = Student.StudentId -- ON helps us identify MATCHING data
+/*  */
 
 --4.	Select the Student full name, courseID's and marks for studentID 199899200.
 /*
