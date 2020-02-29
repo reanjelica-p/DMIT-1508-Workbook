bfbf850999ea1bcbfbaafa9df2550cf969473013
reanjelica-p <59627740+reanjelica-p@users.noreply.github.com>
2020-02-28 20:23:10 -0700

    Update D - Simple Joins Exercise.sql

--- SQL/D - Simple Joins Exercise.sql
+++ SQL/D - Simple Joins Exercise.sql
@@ -48,12 +48,23 @@ ORDER BY 'Staff Full Name', CourseId
 
 --3.	Select all the Club ID's and the Student full names that are in them
 SELECT  FirstName + ' ' + LastName AS 'Full Name',
-        ClubId AS 'Club'
+        ClubId
 FROM    Student 
     INNER JOIN Activity 
         ON Student.StudentID = Activity.StudentID
 
 --4.	Select the Student full name, courseID's and marks for studentID 199899200.
+SELECT FirstName + ' ' + LastName AS 'Full Name',
+<<<<<<< HEAD
+	   CourseId,
+	   Marks
+From   Student
+		INNER JOIN Registration
+			ON Registration.StudentID = Student.StudentID
+			ON Registration.CourseId = Course.CourseId
+
+
+
 /*
 SELECT  S.FirstName + ' ' + S.LastName AS 'Student Name',
         R.CourseId,
@@ -63,21 +74,77 @@ FROM    Registration R
             ON S.StudentID = R.StudentID
 WHERE   S.StudentID = 199899200
 */
+=======
+	   CourseID,
+	   Mark
+FROM   Student
+	INNER JOIN Registration
+		ON Student.StudentID = Registration.StudentID
+WHERE Student.StudentID = 199899200
+>>>>>>> 49963b6958216948f905df9f5a8249dbefeda5fc
 
 --5.	Select the Student full name, course names and marks for studentID 199899200.
--- TODO: Student Answer Here...
+SELECT FirstName + ' ' + LastName AS 'Full Name',
+	   CourseName AS 'Course Name',
+	   Mark
+FROM Registration
+	INNER JOIN Student
+		ON Registration.StudentID = Student.StudentID
+	INNER JOIN Course
+		ON Registration.CourseID = Course.CourseID
+WHERE Student.StudentID = 199899200
 
---6.	Select the CourseID, CourseNames, and the Semesters they have been taught in
--- TODO: Student Answer Here...
+--6.	Select the CourseID, CourseNames, and the Semesters they have been taught in --StudentID 199899200?
+SELECT FirstName + ' ' + LastName AS 'Full Name',
+	   R.CourseID,
+	   CourseName AS 'Course Name',
+	   Semester
+FROM Registration AS R
+	INNER JOIN Student	
+		ON R.StudentID = Student.StudentID
+	INNER JOIN Course
+		ON R.CourseID = Course.CourseID
+WHERE Student.StudentID = 199899200
 
 --7.	What Staff Full Names have taught Networking 1?
--- TODO: Student Answer Here...
+SELECT St.FirstName + ' ' + St.LastName AS 'Full Name',
+		CourseName AS 'Course Name'
+FROM Registration AS R
+	INNER JOIN Course
+		ON R.CourseID = Course.CourseID
+	INNER JOIN Staff AS St
+		ON St.StaffID = R.StaffID
+WHERE CourseName LIKE '%Networking_1%'
 
 --8.	What is the course list for student ID 199912010 in semester 2001S. Select the Students Full Name and the CourseNames
--- TODO: Student Answer Here...
+SELECT FirstName + ' ' + LastName AS 'Full Name',
+	   CourseName AS 'Course Name'
+FROM Registration AS R
+	INNER JOIN Course
+		ON R.CourseID = Course.CourseID
+	INNER JOIN Student
+		ON R.StudentID = Student.StudentID
+WHERE R.StudentID = 199912010 AND R.Semester LIKE '%2001S%'
 
 --9. What are the Student Names, courseID's with individual Marks at 80% or higher? Sort the results by course.
--- TODO: Student Answer Here...
+SELECT FirstName + ' ' + LastName As 'Full Name',
+	   R.CourseID,
+	   R.Mark
+FROM Registration AS R
+	INNER JOIN Student
+		ON Student.StudentID = R.StudentId
+	INNER JOIN Course
+		On R.CourseID = Course.CourseID
+WHERE R.Mark >= 80
 
 --10. Modify the script from the previous question to show the Course Name along with the ID.
--- TODO: Student Answer Here...
+SELECT FirstName + ' ' + LastName As 'Full Name',
+	   R.CourseID,
+	   CourseName AS 'Course Name',
+	   R.Mark
+FROM Registration AS R
+	INNER JOIN Student
+		ON Student.StudentID = R.StudentId
+	INNER JOIN Course
+		On R.CourseID = Course.CourseID
+WHERE R.Mark >= 80
